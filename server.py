5cc0beda529abeb0fc5b8fde67d31eefcc187bd7
dilu <dildiluvis@gmail.com>
2021-12-29 04:15:33 +0530

    test

--- server.py
+++ server.py
@@ -75,12 +75,9 @@ def submit_form():
 			#print(email,subject,message)
 			data = request.form.to_dict()
 			# print(data)
-			if len(data['name']) == 0 and len(data['email']) == 0:
-				return render_template('blank_contact.html')
-			else:
-				write_to_file(data)
-				write_to_csv(data)
-				return render_template('thankyou.html',name=name)
+			write_to_file(data)
+			write_to_csv(data)
+			return render_template('thankyou.html',name=name)
 		except:
 			return 'Did not save to Database'
 	else:
